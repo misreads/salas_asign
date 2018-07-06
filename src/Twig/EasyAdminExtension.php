d5fe3a28e84f5ce896da0175656c6e21204cfb1b
Joaquin Acuña <misreads@gmail.com>
2018-07-05 21:29:29 -0400

    EasyAdminFilters - Filtros en algunas vistas de entidades

--- src/Twig/EasyAdminExtension.php
+++ src/Twig/EasyAdminExtension.php
@@ -3,6 +3,8 @@
 namespace App\Twig;
 
 
+use App\Entity\Career;
+use App\Entity\Professor;
 use App\Entity\Section;
 
 class EasyAdminExtension extends \Twig_Extension
@@ -11,15 +13,40 @@ class EasyAdminExtension extends \Twig_Extension
     {
         return [
             new \Twig_SimpleFilter(
-                'filter_admin_actions',
-                [$this, 'filterActions']
-            )
+                'filter_admin_actions_section',
+                [$this, 'filterActionsSection']
+            ),
+            new \Twig_SimpleFilter(
+                'filter_admin_actions_career',
+                [$this, 'filterActionsCareer']
+            ),
+            new \Twig_SimpleFilter(
+                'filter_admin_actions_professor',
+                [$this, 'filterActionsProfessor']
+            ),
         ];
     }
 
-    public function filterActions(array $itemActions, $item)
+    public function filterActionsSection(array $itemActions, $item)
     {
         if ($item instanceof Section && $item->getIsTaken() === true) {
+            unset($itemActions['delete']);
+            unset($itemActions['edit']);
+        }
+        return $itemActions;
+    }
+
+    public function filterActionsCareer(array $itemActions, $item)
+    {
+        if ($item instanceof Career && count($item->getSubjects()) > 0) {
+            unset($itemActions['delete']);
+            unset($itemActions['edit']);
+        }
+        return $itemActions;
+    }
+    public function filterActionsProfessor(array $itemActions, $item)
+    {
+        if ($item instanceof Professor && count($item->getSections()) > 0) {
             unset($itemActions['delete']);
         }
         return $itemActions;
